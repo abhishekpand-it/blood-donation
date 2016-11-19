a0a616aa1ba47e2c5929a73b9d80362f521b43de
Abhishek Pandit <abhishekpandit@Abhisheks-MacBook-Pro.local>
2016-11-19 21:12:55 +0530

    for heroku

--- code/server/services/db.js
+++ code/server/services/db.js
@@ -5,8 +5,10 @@ exports.connect = function (callback) {
 	if (mongo.DB) {
 		return mongo.DB;
 	} else {
-		const url = process.env.MONGOLAB_URI || process.env.DB || process.env.npm_package_config_db;
-		
+		console.log("PRINT VAR");
+		console.log(process.env.npm_package_config_db);
+		//const url = process.env.npm_package_config_db || process.env.MONGOLAB_URI || process.env.DB;
+		const url = "mongodb://cyberlord92:overover123@ds157667.mlab.com:57667/blood-donation-management";
 		if (!url)  {
 			console.error('No database url specified. Use env DB, or package.json.');
 			process.exit(1);
