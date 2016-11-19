5012717c100e8d6cf5a0b8c9810546012a9ae8d7
Abhishek Pandit <abhishekpandit@Abhisheks-MacBook-Pro.local>
2016-11-19 18:45:36 +0530

    master revert

--- code/server/services/db.js
+++ code/server/services/db.js
@@ -5,10 +5,8 @@ exports.connect = function (callback) {
 	if (mongo.DB) {
 		return mongo.DB;
 	} else {
-		//console.log("PRINT VAR");
-		console.log(process.env.npm_package_config_db);
-		//const url = process.env.npm_package_config_db || process.env.MONGOLAB_URI || process.env.DB;
-		const url = "mongodb://cyberlord92:overover123@ds157667.mlab.com:57667/blood-donation-management";
+		const url = process.env.MONGOLAB_URI || process.env.DB || process.env.npm_package_config_db;
+		
 		if (!url)  {
 			console.error('No database url specified. Use env DB, or package.json.');
 			process.exit(1);
